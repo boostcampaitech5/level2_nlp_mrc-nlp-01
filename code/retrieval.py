b1e20c83d8627839082418132bb8aa8a02631774
MuHyeonSon <smuhyeon84@gmail.com>
2023-06-22 17:46:40 +0000

    feat) retriever_ensemble 클래스 추가

--- code/retrieval.py
+++ code/retrieval.py
@@ -3,7 +3,7 @@ import os
 import pickle
 import time
 from contextlib import contextmanager
-from typing import List, Optional, Tuple, Union
+from typing import Callable, List, Optional, Tuple, Union
 
 import faiss
 import numpy as np
@@ -11,8 +11,9 @@ import pandas as pd
 from datasets import Dataset, concatenate_datasets, load_from_disk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tqdm.auto import tqdm
-from rank_bm25 import BM25Okapi
+from rank_bm25 import BM25Okapi, BM25Plus, BM25L
 from elasticsearch_client import ElasticsearchClient
+from sklearn.preprocessing import MinMaxScaler
 
 
 @contextmanager
@@ -422,3 +423,107 @@ if __name__ == "__main__":
 
         with timer("single query by exhaustive search"):
             scores, indices = retriever.retrieve(query)
+
+
+class Retriever_Ensemble(Retrieval):
+    def __init__(self, tokenize_fn: Callable[[str], List[str]], datasets, topk: int, data_path: str = "../data", context_path: str = "wikipedia_documents.json",):
+        self.tokenize_fn = tokenize_fn
+        self.datasets = datasets["validation"]
+        self.data_path = data_path
+        self.topk = topk
+        self.context_path = context_path
+        with open(os.path.join(data_path, context_path), "r", encoding="utf-8") as f:
+            wiki = json.load(f)
+        self.contexts = list(dict.fromkeys([v["text"] for v in wiki.values()]))  # set 은 매번 순서가 바뀌므로
+
+        self.ids = list(range(len(self.contexts)))
+        self.tf_idf = SparseRetrieval(tokenize_fn=tokenize_fn, data_path=self.data_path, context_path=self.context_path)
+        self.bm25 = BM25(tokenize_fn=tokenize_fn, data_path=self.data_path, context_path=self.context_path)
+        self.bm25_plus = BM25_Plus(tokenize_fn=tokenize_fn, data_path=self.data_path, context_path=self.context_path)
+
+        self.tf_idf.get_sparse_embedding()
+        self.bm25.get_sparse_embedding()
+        self.bm25_plus.get_sparse_embedding()
+
+        self.tf_idf_scores, self.tf_idf_indices = self.tf_idf.get_relevant_doc_bulk(self.datasets["question"], k=self.topk)
+        self.bm25_scores, self.bm25_indices = self.bm25.get_relevant_doc_bulk(self.datasets["question"], k=self.topk)
+        self.bm25_plus_scores, self.bm25_plus_indices = self.bm25_plus.get_relevant_doc_bulk(self.datasets["question"], k=self.topk)
+    
+    def ensemble_and_rerank(self):
+        '''추출된 TF-IDF, BM25, BM25+ 각각의 top-k passage들의 score를 Min-Max 정규화하여 모두 합친 뒤, 다시 top-k를 선정하여 retrieve된 passage들을 pd.DataFrame으로 반환합니다'''
+        all_selected_scores, all_selected_indices = [], []
+        
+        # 전체 test_data에 대한 각각의 top-k passage들의 score에 min max scaling를 적용시킵니다. 
+        for i in range(len(self.datasets)):
+            tf_idf_scores = np.array(self.tf_idf_scores[i],float).reshape(-1, 1)
+            bm25_scores = np.array(self.bm25_scores[i],float).reshape(-1, 1)
+            bm25_plus_scores = np.array(self.bm25_plus_scores[i],float).reshape(-1, 1)
+
+            scaler_tf_idf = MinMaxScaler()
+            scaler_bm25 = MinMaxScaler()
+            scaler_bm25_plus = MinMaxScaler()
+
+            scaler_tf_idf.fit(tf_idf_scores)
+            scaler_bm25.fit(bm25_scores)
+            scaler_bm25_plus.fit(bm25_plus_scores)
+
+            scaled_tf_idf_scores = (scaler_tf_idf.transform(tf_idf_scores)).flatten().tolist()  
+            scaled_bm25_scores = (scaler_bm25.transform(bm25_scores)).flatten().tolist()
+            scaled_bm25_plus_scores = (scaler_bm25_plus.transform(bm25_plus_scores)).flatten().tolist()
+            
+            # 각각의 min max scaling이 적용된 passage들을 한 곳에 합칩니다.합쳐진 전체 passage에서 top-k를 선정합니다.
+            scaled_scores = scaled_tf_idf_scores + scaled_bm25_scores + scaled_bm25_plus_scores
+            scaled_indices = self.tf_idf_indices[i] + self.bm25_indices[i] + self.bm25_plus_indices[i]
+            
+            # 합쳐진 전체 passage들을 min max scaling된 score를 기준으로 하여 내림차순으로 정렬합니다.
+            score_index_pairs = list(zip(scaled_scores, scaled_indices))
+            sorted_pairs = sorted(score_index_pairs, key=lambda pair: pair[0], reverse=True)
+            selected_indices = []
+            selected_scores = []
+
+            # 정렬된(rerank된) passage들에 대해 중복되지 않게 top-k 개를 추출합니다.
+            for score, index in sorted_pairs:
+                if index not in selected_indices:
+                    selected_indices.append(index)
+                    selected_scores.append(score)
+
+                    if len(selected_indices) >= self.topk:
+                        break
+
+            if len(selected_indices) < self.topk:
+                raise ValueError("The number of selected documents is less than top-k.")
+                
+            all_selected_scores.append(selected_scores)
+            all_selected_indices.append(selected_indices)
+ 
+        # ensemble을 통해 rerank된 top-k개의 passage들을 반환합니다.
+        if isinstance(self.datasets, str):
+            doc_scores, doc_indices = all_selected_scores, all_selected_indices
+            for i in range(self.topk):
+                print(f"Top-{i+1} passage with score {doc_scores[i]:4f}")
+                print(self.contexts[doc_indices[i]])
+
+            return (doc_scores, [self.contexts[doc_indices[i]] for i in range(self.topk)])
+
+        elif isinstance(self.datasets, Dataset):
+            # Retrieve한 Passage를 pd.DataFrame으로 반환합니다.
+            total = []
+            with timer("query exhaustive search"):
+                doc_scores, doc_indices = all_selected_scores, all_selected_indices
+            for idx, example in enumerate(tqdm(self.datasets, desc="Sparse retrieval: ")):
+                tmp = {
+                    # Query와 해당 id를 반환합니다.
+                    "question": example["question"],
+                    "id": example["id"],
+                    # Retrieve한 Passage의 id, context를 반환합니다.
+                    "context_id": doc_indices[idx],
+                    "context": " ".join([self.contexts[pid] for pid in doc_indices[idx]]),
+                }
+                if "context" in example.keys() and "answers" in example.keys():
+                    # validation 데이터를 사용하면 ground_truth context와 answer도 반환합니다.
+                    tmp["original_context"] = example["context"]
+                    tmp["answers"] = example["answers"]
+                total.append(tmp)
+
+            cqas = pd.DataFrame(total)
+            return cqas
